187dbf8cdcae14aa42dd539657a1f2f32669d4d6
Shauna Dunn <shaunadunn1@gmail.com>
2020-12-04 14:54:00 -0500

    for loops and comments added

--- script.js
+++ script.js
@@ -12,7 +12,8 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
+// Prompts and confirm statements regarding parameters for password
+// Here I used parseInt to convert the string containing an integer into an actual integer
 function generatePassword() {
     var charLengthMin = parseInt(prompt("Enter the minumum number of characters for desired password"));
     var charLengthMax = parseInt(prompt("Enter the maxiumum number of characters for desired password"));
@@ -20,29 +21,64 @@ function generatePassword() {
     var includeUppercase = confirm("Include uppercase?");
     var includeSymbols = confirm("Include symbols?");
     var includeNumbers = confirm("Include numbers?");
-  
-    var password = ""
-  
-    if (includeLowercase === true) {
-      var i = 0; i < 8; i++;
-      getRandomLower()
-      password += 
+// Control flow to make sure user chooses at least one character type
+    if (
+        includeLowercase === false &&
+        includeUppercase === false &&
+        includeSymbols === false &&
+        includeNumbers === false
+    ) {
+        alert("Password must include a character type!");
     }
-  }
+    // Runs function again
+    generatePassword();
+}
+    // Setting up password length to be between the min and max
+    var passwordLength = generatePasswordLength(charLengthMin, charLengthMax);
+    console.log(passwordLength);
+    // Empty string to which we will append the random characters
+    var password = ""
+  // for loop which will add new characters to our string until the length requirements are met
+    for (var i = 0; i < passwordLength; i++) {
+  // all if statements run independently of one another
+  // each loop creates 4 characters if the user chooses to include all types
+    if (includeLowercase === true) {
+        password += randomLower();
+    }
+    if (includeUppercase === true) {
+        password += randomUpper();
+    }
+    if (includeSymbols === true) {
+        password += randomSymbol();
+    }
+    if (includeNumbers === true) {
+        password += randomNumber();    
+    }
+    }
+// This statement takes a slice from the password string that is exactly the password length
+    var passwordText = password.slice(0, passwordLength);
+// grabs the password display element and assigns the password to text content, thus displaying it
+    document.querySelector("#password").textContent = passwordText;
+}
+
+// function creates a random number within a given range using Math.floor
+function generatePasswordLength(charLengthMin, charLengthMax) {
+    return Math.floor(Math.random() * (maxLength - minLength + 1) + minLength);
+}
   
   //Password generator functions
-  function getRandomLower() {
+  function randomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
   }
   
-  function getRandomUpper() {
+  function randomUpper() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
   }
   
-  function getRandomNumber() {
-    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+  function randomNumber() {
+    return Math.floor(Math.random() * 10).toString();
   }
   
-  function getRandomSymbol() {
-    const symbols = "!@#$%^&*()[]{}<>?/";
+  function randomSymbol() {
+    var symbols = "!@#$%^&*()[]{}<>?/";
     return symbols[Math.floor(Math.random() * symbols.length)];
