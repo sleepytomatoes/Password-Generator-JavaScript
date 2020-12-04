cebd312e1c62068b411d1c23ec7e20f0b2cc6115
Shauna Dunn <shaunadunn1@gmail.com>
2020-12-04 13:09:05 -0500

    password generator functions and prompts/confirms

--- script.js
+++ script.js
@@ -12,3 +12,37 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+    var charLengthMin = parseInt(prompt("Enter the minumum number of characters for desired password"));
+    var charLengthMax = parseInt(prompt("Enter the maxiumum number of characters for desired password"));
+    var includeLowercase = confirm("Include lowercase?");
+    var includeUppercase = confirm("Include uppercase?");
+    var includeSymbols = confirm("Include symbols?");
+    var includeNumbers = confirm("Include numbers?");
+  
+    var password = ""
+  
+    if (includeLowercase === true) {
+      var i = 0; i < 8; i++;
+      getRandomLower()
+      password += 
+    }
+  }
+  
+  //Password generator functions
+  function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  }
+  
+  function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+  }
+  
+  function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+  }
+  
+  function getRandomSymbol() {
+    const symbols = "!@#$%^&*()[]{}<>?/";
+    return symbols[Math.floor(Math.random() * symbols.length)];
